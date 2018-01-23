1a10faef166ffec5b71d9712011c53743599cdb2
stereobooster <stereobooster@gmail.com>
2018-01-23 15:27:55 +0100

    @fontface testcase

--- tests/main.test.js
+++ tests/main.test.js
@@ -88,3 +88,22 @@ test('removes used inline @keyframes', async () => {
   const { finalCss } = await runMinimalcss('keyframe-removes-inline')
   expect(finalCss).toEqual(result)
 })
+
+test('removes unused @fontface', async () => {
+  const result = ''
+  const { finalCss } = await runMinimalcss('fontface-removes')
+  expect(finalCss).toEqual(result)
+})
+
+test('leaves used @fontface', async () => {
+  const result =
+    "@font-face{font-family:'Lato';font-style:normal;font-weight:400;src:local('Lato Regular'),local('Lato-Regular'),url(https://fonts.gstatic.com/s/lato/v14/MDadn8DQ_3oT6kvnUq_2r_esZW2xOQ-xsNqO47m55DA.woff2) format('woff2');unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2212,U+2215}.SomeSelector{font-family:'Lato'}"
+  const { finalCss } = await runMinimalcss('fontface-leaves')
+  expect(finalCss).toEqual(result)
+})
+
+test('removes used inline @fontface', async () => {
+  const result = ''
+  const { finalCss } = await runMinimalcss('fontface-removes-inline')
+  expect(finalCss).toEqual(result)
+})
