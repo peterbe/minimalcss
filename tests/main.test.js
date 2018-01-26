a0ed814fa3291a5773896c36ce5d0c977e502571
stereobooster <stereobooster@gmail.com>
2018-01-26 19:05:42 +0100

    Refactor tests (#111)
    * Refactor tests
    * skip not working cases

--- tests/main.test.js
+++ tests/main.test.js
@@ -50,9 +50,8 @@ test('handles JS errors', async () => {
 })
 
 test('cares only about external CSS files', async () => {
-  const result = '.external{color:red}'
   const { finalCss } = await runMinimalcss('css-in-js')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toEqual('.external{color:red}')
 })
 
 test('handles 404 CSS file', async () => {
@@ -65,72 +64,64 @@ test('handles 404 CSS file', async () => {
 })
 
 test('media queries print removed', async () => {
-  const result = ''
   const { finalCss } = await runMinimalcss('media-queries-print')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toEqual('')
 })
 
 test('removes unused @keyframes', async () => {
-  const result = ''
   const { finalCss } = await runMinimalcss('keyframe-removes')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toEqual('')
 })
 
 test('leaves used @keyframes', async () => {
-  const result =
-    '@keyframes RotateSlot{3%{margin-top:-2em}0%{transform:rotate(0deg)}}.SomeSelector{animation:RotateSlot infinite 5s linear}'
   const { finalCss } = await runMinimalcss('keyframe-leaves')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toMatch('@keyframes RotateSlot')
 })
 
-test('removes used inline @keyframes', async () => {
-  const result = ''
+test.skip('leaves used inline @keyframes', async () => {
   const { finalCss } = await runMinimalcss('keyframe-removes-inline')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toMatch('@keyframes RotateSlot')
 })
 
 test('removes unused @fontface', async () => {
-  const result = ''
   const { finalCss } = await runMinimalcss('fontface-removes')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toEqual('')
 })
 
 test('leaves used @fontface', async () => {
-  const result =
-    "@font-face{font-family:'Lato';font-style:normal;font-weight:400;src:local('Lato Regular'),local('Lato-Regular'),url(https://fonts.gstatic.com/s/lato/v14/MDadn8DQ_3oT6kvnUq_2r_esZW2xOQ-xsNqO47m55DA.woff2) format('woff2');unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2212,U+2215}.SomeSelector{font-family:'Lato'}"
   const { finalCss } = await runMinimalcss('fontface-leaves')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toMatch("@font-face{font-family:'Lato';")
 })
 
-test('removes used inline @fontface', async () => {
-  const result = ''
+test.skip('leaves used inline @fontface', async () => {
   const { finalCss } = await runMinimalcss('fontface-removes-inline')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toMatch("@font-face{font-family:'Lato';")
 })
 
 test('leaves used pseudo classes', async () => {
-  const result =
-    'a{color:red}a:active{color:olive}input:disabled{color:red}a:focus{color:green}a:hover{color:#00f}a:visited{color:orange}'
   const { finalCss } = await runMinimalcss('pseudo-classes')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toMatch('a:active')
+  expect(finalCss).toMatch('a:focus')
+  expect(finalCss).toMatch('a:hover')
+  expect(finalCss).toMatch('a:visited')
+  expect(finalCss).toMatch('input:disabled')
 })
 
 test('media queries', async () => {
-  const result =
-    '@media only screen and (min-device-width:414px) and (max-device-width:736px) and (-webkit-min-device-pixel-ratio:3){a{color:red}}@media only screen and (min-device-width:375px) and (max-device-width:812px) and (-webkit-min-device-pixel-ratio:3){a{color:green}}'
   const { finalCss } = await runMinimalcss('media-queries')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toMatch('@media only screen and (min-device-width:414px)')
+  expect(finalCss).toMatch('@media only screen and (min-device-width:375px)')
 })
 
 test('evaluate DOM multiple times', async () => {
-  const result = '.SomeSelector{color:red}.OtherSelector{background:#000}'
   const { finalCss } = await runMinimalcss('evaluate-dom-multiple-times')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toMatch('.SomeSelector')
+  expect(finalCss).toMatch('.OtherSelector')
 })
 
 test('form elements', async () => {
-  const result =
-    'input[type=radio]:checked{color:red}input[type=checkbox]:checked{color:#00f}option:selected{color:green}'
   const { finalCss } = await runMinimalcss('form-elements')
-  expect(finalCss).toEqual(result)
+  expect(finalCss).toMatch('input[type=radio]:checked')
+  expect(finalCss).toMatch('input[type=checkbox]:checked')
+  expect(finalCss).toMatch('option:selected')
 })
