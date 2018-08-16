ddad6a09a06b4badad099d118fe16e259b8b33ef
Peter Bengtsson <mail@peterbe.com>
2018-08-16 16:55:12 -0400

    tests for utility functions

---
+++ tests/utils.test.js
@@ -0,0 +1,21 @@
+const utils = require('../src/utils');
+
+test('Test unquoteString', async () => {
+  const f = utils.unquoteString;
+  expect(f('"peter"')).toEqual('peter');
+  expect(f(`'peter'`)).toEqual('peter');
+  // but leave it if it isn't balanced
+  expect(f('"peter')).toEqual('"peter');
+  // empty strings
+  expect(f('')).toEqual('');
+});
+
+test('Test reduceCSSSelector', async () => {
+  const f = utils.reduceCSSSelector;
+  // Most basic case.
+  expect(f('a:hover')).toEqual('a');
+  // More advanced case.
+  expect(f('a[href^="javascript:"]:after')).toEqual('a[href^="javascript:"]');
+  // Should work with ' instead of " too.
+  expect(f("a[href^='javascript:']:after")).toEqual("a[href^='javascript:']");
+});
