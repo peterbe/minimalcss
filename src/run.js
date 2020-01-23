477c54685b30fbc23b4f3c7fa0e914161860a835
Gijo Varghese <gijovarghese141@gmail.com>
2020-01-23 10:58:33 +0530

    adding htmlContents

--- src/run.js
+++ src/run.js
@@ -406,7 +406,7 @@ const processPage = ({
 /**
  *
  * @param {{ urls: Array<string>, debug: boolean, loadimages: boolean, skippable: function, browser: any, userAgent: string, withoutjavascript: boolean, viewport: any, puppeteerArgs: Array<string>, cssoOptions: Object, ignoreCSSErrors?: boolean, ignoreJSErrors?: boolean, styletags?: boolean, enableServiceWorkers?: boolean, disableJavaScript?: boolean, whitelist?: Array<string> }} options
- * @return Promise<{ finalCss: string, stylesheetContents: { [key: string]: string } }>
+ * @return Promise<{ finalCss: string, stylesheetContents: { [key: string]: string }, htmlContents: Array<string> }>
  */
 const minimalcss = async options => {
   const { urls } = options;
@@ -656,9 +656,11 @@ const minimalcss = async options => {
   csso.syntax.compress(allCombinedAst, cssoOptions);
   postProcessOptimize(allCombinedAst);
   const finalCss = csstree.generate(allCombinedAst);
+  const htmlContents = doms.map(dom => dom.html());
   const returned = {
     finalCss,
-    stylesheetContents
+    stylesheetContents,
+    htmlContents
   };
   return Promise.resolve(returned);
 };
